f9d2973e024a5f697bb93659c32bae662354b937
Ashley Valentine <avalentinexo@gmail.com>
2023-02-06 16:55:12 -0500

    added ability to store and emit user data

--- server.js
+++ server.js
@@ -3,8 +3,35 @@ const io = require('socket.io')(3050, {
     origin: ['http://localhost:5173']
   }
 })
+const { addUser, getUser, deleteUser, getUsers } = require('./users')
 
 io.on('connection', (socket) => {
-  const room = socket.handshake.query.room
-  socket.join(room)
+  socket.on('add-user', ({ userName, userRoom }, callback) => {
+    const { error, user } = addUser(socket.id, userName, userRoom)
+
+    console.log(user)
+
+    if (error) return callback(error)
+
+    socket.join(user.room)
+
+    io.to(user.room).emit('room-data', {
+      room: user.room,
+      userData: getUsers(user.room)
+    })
+
+    callback({ status: 'ok' })
+  })
+
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+    const user = deleteUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('room-data', {
+        room: user.room,
+        userData: getUsers(user.room)
+      })
+    }
+  })
 })
